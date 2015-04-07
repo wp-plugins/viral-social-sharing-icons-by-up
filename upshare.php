34cdf3b9a5ee0240f136b88aed81577213e92bec
upshare <upshare@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-07 14:45:06 +0000

    updated
    git-svn-id: https://plugins.svn.wordpress.org/viral-social-sharing-icons-by-up/trunk@1129639 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- upshare.php
+++ upshare.php
@@ -241,7 +241,7 @@ class UP_ViralSharingSocial
 
 function viral_share_footer(){
 
-echo '<!-- Viral Social Sharing Buttons by www.upshare.co -->';
+echo '<!-- Viral Social Sharing by www.upshare.co, more info here https://www.upshare.co/partners/sign_up -->';
 echo '<link rel="stylesheet" href="//assets.upshare.co/widget-b/widget.css" type="text/css" media="all" />';
 echo '<div id="up-branding">Viral <a target="_blank" href="http://www.upshare.co/buttons/">Buttons</a> by UP</div>';
 echo '<script src="//widget.upshare.co/up-load.js?signupArrow=true&cms=wp" id="UPWidget"></script>';
